90849ab2e7c23864d8e1f7abc42ee6897c3e6857
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-10-13 21:02:03 +0100

    brought in files and folders for http server

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,8 +1,26 @@
+from datetime import datetime
+from enum import Enum
+from json import loads
+from uuid import UUID
+
 from pydantic import BaseModel, Field
 from core.enums import ActionType
 
 
-class Action(BaseModel):
+class CustomBaseModel(BaseModel):
+    model_config = {
+        "json_encoders": {
+            UUID: str,
+            datetime: lambda dt: dt.isoformat(),
+            Enum: lambda e: e.value,
+        }
+    }
+
+    def to_serialisable_dict(self) -> dict:
+        return loads(self.model_dump_json())
+
+
+class Action(CustomBaseModel):
     type: ActionType
     reason: str
 
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,8 @@
+import uvicorn
+
+
 def main():
-    print("Hello from ai-chat-moderation!")
+    uvicorn.run("server.app:app", host="localhost", port=8000, reload=True)
 
 
 if __name__ == "__main__":
